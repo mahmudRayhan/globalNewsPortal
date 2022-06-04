88963cda91ca4eb0de5211e32ebd50e369d3aa0f
Mahmud Rayhan <1605035@ugrad.cse.buet.ac.bd>
2022-06-04 21:48:15 +0600

    make it better

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,22 @@
 var mongoose = require('mongoose')
 
+/*
 const databaseURL ="mongodb://localhost:27017/globalNews"
 
 mongoose.connect(databaseURL
 ,{useNewUrlParser:true, useUnifiedTopology:true}).then(
     ()=>console.log("Connection Successful!")
-).catch( (error) => console.log(err));
+).catch( (error) => console.log(error));
+
+*/
+
+const databaseURL ='mongodb+srv://user01:987654321@globalnews.22wx3.mongodb.net/globalNews_database?retryWrites=true&w=majority/';
+mongoose.connect(databaseURL,{
+        useNewUrlParser:true, 
+        useCreateIndex: true,
+        useFindAndModify: false ,
+        useUnifiedTopology:true
+    }).then(
+        ()=>console.log("Connection Successful with Mongodb atlas!")
+    ).catch( (error) => console.log(error));
+
